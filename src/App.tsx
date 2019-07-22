5f99570ba7d099d71cb72959bbc976f9d982b40a
Johan Ho <johan.ho@gmail.com>
2019-07-22 21:28:12 +0200

    Migrated App.tsx to React Hooks instead of component class.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { useState, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import ScrollToTop from './ScrollToTop';
 import TopBar from './TopBar';
@@ -8,43 +8,39 @@ import CampInfo from './CampInfo';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import './scss/App.scss';
 
-class App extends React.Component {
-  state = {
-    redirect: '',
-    error: null,
-    sectionSelectedIndex: 0,
-    campLoadedId: 0,
-    campName: '',
-    campData: {},
-    campConfig: []
-  };
-  
-  constructor( props ) {
-    super( props );
+export default function App() {
+  console.log('Camp App: Rendering App...');
+    
+  const [campData, setCampData] = useState<{ ID: number; Name: string; Sheets: {}; Config: any[]; }>( {
+    ID: 0,
+    Name: '',
+    Sheets: {},
+    Config: []
+  } );
 
-    let pathname = localStorage.getItem('pathname');
-    if (pathname) {
-      console.log('Camp App: First load and pathname is set, loading saved pathname:',pathname);
-      this.state.redirect = pathname;
-    } else {
-      console.log('Camp App: First load - No saved pathname, do nothing.' );
-    }
+  let redirect = '';
+  let pathname = localStorage.getItem('pathname');
+  if (pathname) {
+    console.log('Camp App: First load and pathname is set, loading saved pathname:',pathname);
+    redirect = pathname;
+  } else {
+    console.log('Camp App: First load - No saved pathname, do nothing.' );
   }
 
-  saveRoute(route) {
+  function saveRoute(route) {
     let pathname = route.location.pathname;
     console.log('Camp App: Saving pathname: ', pathname);
     localStorage.setItem( 'pathname', pathname );
   }
 
-  loadCampInfo = (campid) => {
+  const loadCampInfo = (campid) => {
     console.log('Camp App: Loading info for camp selected: '+campid);
     fetch('https://api.nccc.se/camps/'+campid)
     .then(result => result.json())
     .then(
       (data) => {
         console.log('Data fetched complete for: '+campid);
-        let campData = {};
+        let sheets = {};
         let config: any[] = [];
         if (data.sheets) {
           for (let sheet of data.sheets) {
@@ -80,131 +76,119 @@ class App extends React.Component {
             if (sheet.properties.title === 'CONFIG') {
               config = parsedData;
             } else {
-              campData[sheet.properties.title] = parsedData;
+              sheets[sheet.properties.title] = parsedData;
             }
           }
-          this.setState({
-            campLoadedId: campid,
-            campName: data.properties.name,
-            campData: campData,
-            campConfig: config
-          });
+          setCampData( {
+            ID: campid,
+            Name: data.properties.name,
+            Sheets: sheets,
+            Config: config
+          } );
         } else {
-          let error = 'Camp App: Error when fetching camp data - Invalid data received.';
-          this.setState({campLoadedId: campid, campData: null, campConfig: null, error});
-          console.log(error);
+          setCampData( {
+            ID: campid,
+            Name: '',
+            Sheets: {},
+            Config: []
+          } );
+          console.log('Camp App: Error when fetching camp data - Invalid data received.');
         }
       },
       (error) => {
-        this.setState({campLoadedId: campid, campData: null, campConfig: null, error});
-        console.log('Camp App:  Fetching camp info failed:', error);
+          setCampData( {
+            ID: campid,
+            Name: '',
+            Sheets: {},
+            Config: []
+          } );
+        console.log('Camp App: Fetching camp info failed:', error);
       }
     );
   };
   
-  findSheet = ( sheetName ) => {
-    let data: any = this.state.campData;
-    if (data) {
-      for (let sheet of data.sheets) {
-        if (sheet.properties.title === sheetName) {
-          return sheet;
-        }
-      }
-    }
-    return null;
-  }
-  
-  render() {
-    const { campName, redirect } = this.state
-    
-    console.log('Camp App: Rerendering...');
-    
-    return (
-      <Router>
-        <Suspense fallback={<CircularProgress />}>
-          { redirect
-            ? <Redirect
-                to={redirect}
-              />
-            : null
-          }
-          <ScrollToTop>
-            <Route exact path="/"
-              render={routeProps => {
-                this.saveRoute( routeProps )
+  return (
+    <Router>
+      <Suspense fallback={<CircularProgress />}>
+        <ScrollToTop>
+          <Route exact path="/"
+            render={routeProps => {
+              if (redirect) {
+                return (<Redirect
+                          to={redirect}
+                        />)
+              } else {
+                saveRoute( routeProps );
                 return (
                   <div className="App">
                     <div className="App-header">
                       <TopBar
                         campID=""
-                        campName={campName}
+                        campName={campData.Name}
                         sectionName=""
                       />
                     </div>
                     <CampSelect />
                   </div>
-                )}
-              }
-            />
-            <Route path="/:campid/:section?"
-              render={routeProps => {
-                const { campLoadedId, campData, campConfig } = this.state;
-                this.saveRoute( routeProps )
-                let sectionSelected = routeProps.match.params.section;
-                let campid = routeProps.match.params.campid;
-                let sectionIndex = 0;
-                let config: any = campConfig;
-                let sectionConfig: any = null;
-                let sectionName: string = "";
-                let sectionData: any = null;
-                if (campid !== campLoadedId) {
-                  this.loadCampInfo( campid );
-                }
-                else if (campConfig && campData) {
-                  for (let index = 0; index < config.length; index++) {
-                    if (config[index].Name === sectionSelected) {
-                      sectionIndex = index;
-                      break;
-                    }
-                  }
-                  if (campConfig[sectionIndex]) {
-                    sectionConfig = campConfig[sectionIndex];
-                    sectionName = sectionConfig.Name;
-                    sectionData = campData[sectionName];
-                  }
-                }
-                return (
-                <div className="App">
-                  <div className="App-header">
-                    <TopBar
-                      campid={campid}
-                      campname={campName}
-                      sectionname={sectionName}
-                    />
-                  </div>
-                  <div className="App-main">
-                    <CampInfo
-                      isLoaded={campLoadedId===campid}
-                      sectionConfig={sectionConfig}
-                      sectionData={sectionData}
-                    />
-                  </div>
-                  <div className="App-footer">
-                    <BottomBar
-                      index={sectionIndex}
-                      campid={campid}
-                      campConfig={campConfig}
-                    />
-                  </div>
-                </div>
                 )
-              }}
-            />
-          </ScrollToTop>
-        </Suspense>
-      </Router>
-    );
-  }
+              }
+            }}
+          />
+          <Route path="/:campid/:section?"
+            render={routeProps => {
+              saveRoute( routeProps );
+              let sectionSelected = routeProps.match.params.section;
+              let campid = routeProps.match.params.campid;
+              let sectionIndex = 0;
+              let config: any = campData.Config;
+              let sectionConfig: any = null;
+              let sectionName: string = "";
+              let sectionData: any = null;
+              if (campid !== campData.ID) {
+                loadCampInfo( campid );
+              }
+              else if (config.length > 0) {
+                for (let index = 0; index < config.length; index++) {
+                  if (config[index].Name === sectionSelected) {
+                    sectionIndex = index;
+                    break;
+                  }
+                }
+                if (config[sectionIndex]) {
+                  sectionConfig = config[sectionIndex];
+                  sectionName = sectionConfig.Name;
+                  sectionData = campData.Sheets[sectionName];
+                }
+              }
+              return (
+              <div className="App">
+                <div className="App-header">
+                  <TopBar
+                    campid={campid}
+                    campname={campData.Name}
+                    sectionname={sectionName}
+                  />
+                </div>
+                <div className="App-main">
+                  <CampInfo
+                    isLoaded={campData.ID === campid}
+                    sectionConfig={sectionConfig}
+                    sectionData={sectionData}
+                  />
+                </div>
+                <div className="App-footer">
+                  <BottomBar
+                    index={sectionIndex}
+                    campid={campid}
+                    campConfig={campData.Config}
+                  />
+                </div>
+              </div>
+              )
+            }}
+          />
+        </ScrollToTop>
+      </Suspense>
+    </Router>
+  )
 }
-
-export default App;
